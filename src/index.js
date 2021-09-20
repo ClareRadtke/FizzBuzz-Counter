e3c79b2633d19f0a686630f57752c05b86405766
Clare Radtke <clare.radtke@outlook.com>
2021-09-20 19:38:54 +1000

    HTML webpack templating configured, React initiated

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
+import React from "react";
 
+import { render } from "react-dom";
+
+render(<div>Hello!</div>, document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+  ],
   output: {
-    filename: "main.js",
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
@@ -12,6 +18,16 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
       },
     ],
   },
