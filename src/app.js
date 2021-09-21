9decb6f041a93e2bfa53d0e751998500dd879c40
Clare Radtke <clare.radtke@outlook.com>
2021-09-21 15:29:10 +1000

    set count as a variable for use in the onclick functions for each button

--- src/app.js
+++ src/app.js
@@ -2,19 +2,38 @@ import React from "react";
 import styles from "./styles.module.css";
 
 function App() {
+  let count = 0;
   return (
     <div className={styles.main}>
       <div className={styles.buttonContainer}>
-        <button id="increment" className={styles.button} type="button">
+        <button
+          id="increment"
+          className={styles.button}
+          type="button"
+          onClick={(event) => {
+            console.log(count++);
+          }}
+        >
           +
         </button>
-        <button id="decrement" className={styles.button} type="button">
+        <button
+          id="decrement"
+          className={styles.button}
+          type="button"
+          onClick={(event) => {
+            console.log(count--);
+          }}
+        >
           -
         </button>
       </div>
-
-      <div id="text-output" className={styles.text}>
-        text
+      <div className={styles.textContainer}>
+        <p id="text-output" className={styles.text}>
+          count:
+          <span id="count" className={styles.text}>
+            {count}
+          </span>
+        </p>
       </div>
     </div>
   );
