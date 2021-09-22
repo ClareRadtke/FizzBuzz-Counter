ee67e58e923b2b3efab511f0f15551a543254df7
Clare Radtke <clare.radtke@outlook.com>
2021-09-23 09:04:34 +1000

    Code housekeeping

--- src/app.js
+++ src/app.js
@@ -1,18 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import styles from "./styles.module.css";
 
 import { LandingPage } from "./components/LandingPage/landingPage";
 import { LoginPage } from "./components/LoginPage/loginPage";
 
 function App() {
-  const [data, setData] = React.useState(null);
   const [ActivePage, setActivePage] = React.useState("LoginPage");
 
-  React.useEffect(() => {
-    fetch("/api/test")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
   return (
     <div className={styles.main}>
       {ActivePage === "FizzBuzz" ? (
